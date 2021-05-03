1d277ced2a8a898d627b6abaeba88605fa8dd127
Uzichow <quentin.lehoux@outlook.com>
2021-05-03 13:47:47 +0200

    Add scripts

---
+++ UPD receive.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import socket
+
+localIP     = "192.168.1.79"
+localPort   = 32645
+bufferSize  = 1024
+msgFromServer       = "Hello UDP Client"
+path = "UPD_messages.txt"
+
+bytesToSend = str.encode(msgFromServer)
+
+
+# Create a datagram socket
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+
+# Bind to address and ip
+
+UDPServerSocket.bind((localIP, localPort))
+
+print("UDP server up and listening")
+
+# Listen for incoming datagrams
+
+for i in range(10): 
+	
+	date = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')	
+	bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+	
+	message = bytesAddressPair[0]
+	address = bytesAddressPair[1]
+	
+	clientMsg = "Message from Client:{}".format(message)
+	clientIP  = "Client IP Address:{}".format(address)
+	
+	file = open(path,"a")
+	file.write('Date : ' + date + '   Température : ' + format(message) + '°C \n')
+	file.close()
+	
+	print(clientMsg)
+	print(clientIP)
+	i+=1
---
+++ UPD_sending.ino
@@ -0,0 +1,215 @@
+/*
+
+  WiFi UDP Send and Receive String
+
+ This sketch wait an UDP packet on localPort using the WiFi module.
+
+ When a packet is received an Acknowledge packet is sent to the client on port remotePort
+
+ created 30 December 2012
+
+ by dlf (Metodo2 srl)
+
+ */
+
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+
+int status = WL_IDLE_STATUS;
+
+//Initialize to the WIFI settings
+char ssid[] = "SFR_C538";        // your network SSID (name)
+char pass[] = "0493746045";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+unsigned int localPort = 2390;      // local port to listen on
+
+char packetBuffer[256]; //buffer to hold incoming packet
+char  ReplyBuffer[] = "acknowledged";       // a string to send back
+
+WiFiUDP Udp;
+
+float R1 = 10000;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+
+
+void setup() {
+
+  //Initialize serial and wait for port to open:
+
+  Serial.begin(9600);
+
+  while (!Serial) {
+
+    ; // wait for serial port to connect. Needed for native USB port only
+
+  }
+
+  // check for the WiFi module:
+
+  if (WiFi.status() == WL_NO_MODULE) {
+
+    Serial.println("Communication with WiFi module failed!");
+
+    // don't continue
+
+    while (true);
+
+  }
+
+  String fv = WiFi.firmwareVersion();
+
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+
+    Serial.println("Please upgrade the firmware");
+
+  }
+
+  // attempt to connect to Wifi network:
+
+  while (status != WL_CONNECTED) {
+
+    Serial.print("Attempting to connect to SSID: ");
+
+    Serial.println(ssid);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+
+    delay(10000);
+
+  }
+
+  Serial.println("Connected to wifi");
+
+  printWifiStatus();
+
+  Serial.println("\nStarting connection to server...");
+
+  // if you get a connection, report back via serial:
+
+  Udp.begin(localPort);
+}
+
+void loop() {
+
+    for (int i=0;i<10;i++) {
+
+    // Get data from sensor
+    float data = getData();
+
+    // Send data to network
+    sendData(data);
+      
+
+    }
+    
+    
+  // if there's data available, read a packet
+
+  int packetSize = Udp.parsePacket();
+
+  if (packetSize) {
+
+    Serial.print("Received packet of size ");
+
+    Serial.println(packetSize);
+
+    Serial.print("From ");
+
+    IPAddress remoteIp = Udp.remoteIP();
+
+    Serial.print(remoteIp);
+
+    Serial.print(", port ");
+
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+
+    int len = Udp.read(packetBuffer, 255);
+
+    if (len > 0) {
+
+      packetBuffer[len] = 0;
+
+    }
+
+    Serial.println("Contents:");
+
+    Serial.println(packetBuffer);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+
+    Udp.write(ReplyBuffer);
+
+    Udp.endPacket();
+
+  }
+}
+
+void printWifiStatus() {
+
+  // print the SSID of the network you're attached to:
+
+  Serial.print("SSID: ");
+
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+
+  IPAddress ip = WiFi.localIP();
+
+  Serial.print("IP Address: ");
+
+  Serial.println(ip);
+
+  // print the received signal strength:
+
+  long rssi = WiFi.RSSI();
+
+  Serial.print("signal strength (RSSI):");
+
+  Serial.print(rssi);
+
+  Serial.println(" dBm");
+}
+
+float getData() {
+  int Vo = analogRead(0);
+
+  float R2 = R1 * (1023.0 / (float)Vo - 1.0);
+  float logR2 = log(R2);
+
+  float T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2* logR2)) - 273.15;
+  
+  return T;
+}
+
+void sendData(float data) {
+  Serial.print("Temperature: ");
+  
+  Serial.println("°C");
+  Serial.println("Sending UPD packet ...");
+
+  String data_string = String(data);
+
+  char* buf = new char[6];
+  data_string.toCharArray(buf, 6);
+  
+  char* packet =  buf;
+  Serial.println(packet);
+  Udp.beginPacket("192.168.1.79", "2390");
+
+  Udp.write(packet);
+
+  Udp.endPacket();
+  delay(1000);
+  
+}
